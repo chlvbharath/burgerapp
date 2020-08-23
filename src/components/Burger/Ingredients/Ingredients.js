4fa989ea9418789e6a3776d3fdbd985f70d17fb5
Bharath <bharathkumarhoc@gmail.com>
2020-08-23 15:23:15 -0500

    cleaning the code

---
+++ src/components/Burger/Ingredients/Ingredients.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import classes from './Ingredients.module.css'
+import PropTypes from 'prop-types'
+class Ingredient extends Component {
+    render() {
+        let ingredient = null;
+        switch (this.props.type) {
+            case ('breadbottom'): {
+                ingredient = <div className={classes.BreadBottom}></div>;
+                break;
+            }
+            case ('breadtop'): {
+                ingredient = <div className={classes.BreadTop}>
+                    <div className={classes.Seeds1}></div>
+                    <div className={classes.Seeds2}></div>
+                </div>;
+                break;
+            }
+            case ('meat'): {
+                ingredient = <div className={classes.Meat}></div>;
+                break;
+            }
+            case ('cheese'): {
+                ingredient = <div className={classes.Cheese}></div>;
+                break;
+            }
+            case ('salad'): {
+                ingredient = <div className={classes.Salad}></div>;
+                break;
+            }
+            case ('bacon'): {
+                ingredient = <div className={classes.Bacon}></div>;
+                break;
+            }
+            default: {
+                ingredient = null;
+                break;
+            }
+        }
+        return ingredient;
+    }
+}
+Ingredient.propTypes = {
+    type: PropTypes.string.isRequired
+};
+export default Ingredient;
